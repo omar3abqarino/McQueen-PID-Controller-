295228bac49f31da967ca56e24a5cc99bd5b923f
Omar Samy <omar3abqarino@gmail.com>
2026-09-08 23:15:30 +0300

    added error deadzone, edited init and added update

--- main_class.py
+++ main_class.py
@@ -1,7 +1,7 @@
-
+import time
 
 class PID_Controller:
-    def __init__(self, kp, ki, kd, current_state, target = None):
+    def __init__(self, kp, ki, kd, current_state, target = 0.0):
 
         self.kp = kp
         self.ki = ki
@@ -20,4 +20,27 @@ class PID_Controller:
         self.target = target
         print("Target Recieved!!")
 
-    
+    def error_deadzone(self):
+        if abs(self.error) > 1.0:
+            return True
+        else:
+            return 0.0
+
+    def update(self):
+        if not self.error_deadzone():
+            current_time = time.time()
+
+            if not self.last_time:
+                self.last_time = current_time
+                return False
+
+            #dt
+            dt = current_time - self.last_time
+
+            # e(t)
+            self.error = self.current_state - self.target
+
+
+            pterm = self.kp * self.error
+            
+            
