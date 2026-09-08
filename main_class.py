e15eb4fcdad5225170f6d00fe558624de63b2626
Omar Samy <omar3abqarino@gmail.com>
2026-09-08 23:42:50 +0300

    added compute and some edits to general shape

--- main_class.py
+++ main_class.py
@@ -1,17 +1,19 @@
 import time
 
 class PID_Controller:
-    def __init__(self, kp, ki, kd, current_state, target = 0.0):
+    def __init__(self, kp, ki, kd, target = 0.0):
 
         self.kp = kp
         self.ki = ki
         self.kd = kd
 
-        self.current_state = current_state
         self.target = target
 
-        self.error = None
-        self.last_time = None
+        self.current_state = 0.0
+        self.current_error = 0.0
+        self.last_error = 0.0
+        self.last_time = 0.0
+        self.integral = 0.0
         
 
 
@@ -21,26 +23,56 @@ class PID_Controller:
         print("Target Recieved!!")
 
     def error_deadzone(self):
-        if abs(self.error) > 1.0:
-            return True
+        if abs(self.current_error - self.last_error) > 1.0:
+            return self.current_error - self.last_error
         else:
             return 0.0
 
-    def update(self):
-        if not self.error_deadzone():
-            current_time = time.time()
-
-            if not self.last_time:
-                self.last_time = current_time
-                return False
-
-            #dt
-            dt = current_time - self.last_time
-
-            # e(t)
-            self.error = self.current_state - self.target
+    def update_current(self, current_state):
+        self.current_state = current_state
+        print("Current State Recieved!!")
 
 
-            pterm = self.kp * self.error
-            
-            
+    def compute(self):
+        
+        current_time = time.time()
+
+        if not self.last_time:
+            self.last_time = current_time
+            return False
+
+        #dt
+        dt = current_time - self.last_time
+
+        # e(t)
+        self.current_error = self.error_deadzone()
+
+        
+        # d e(t)
+        delta_e = self.current_error - self.last_error
+
+        #integral
+        self.integral += self.current_error * dt
+
+        #pterm
+        pterm = self.kp * self.current_error
+
+        #iterm
+        iterm = self.ki * self.integral
+
+        #dterm
+        dterm = self.kd * (delta_e/dt)
+
+        #final equation
+        u_t = pterm + iterm + dterm
+
+
+        #updating for next iteration
+        self.last_error = self.current_error
+        self.last_time = current_time
+
+        return u_t
+
+
+
+
